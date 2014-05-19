8dc9c2aa9b0f6ecae95f6526e1732ef033d4bc81
RuslanDoel <RuslanDoel@users.noreply.github.com>
2014-05-19 12:15:40 +0700

    home
    php

--- home.php
+++ home.php
@@ -19,12 +19,6 @@ IVERSITAS SILIWANGI
 TUGAS LAYANAN WEB [GITHUB](https://github.com)
 
 
-GitHub adalah layanan berbasis web hosting untuk proyek-proyek pengembangan perangkat lunak yang menggunakan sistem kontrol revisi Git. GitHub menawarkan rencana dibayar untuk repositori pribadi, dan akun gratis untuk proyek-proyek open source. Pada Mei 2011, GitHub adalah sumber yang paling populer kode situs repositori terbuka.
-
-GitHub juga merupakan tempat terbaik untuk berbagi kode dengan teman-teman, rekan kerja, teman sekelas, dan orang asing. Lebih dari dua juta orang menggunakan GitHub untuk membangun hal-hal menakjubkan bersama-sama.
-
-Dengan fitur kolaboratif GitHub.com, desktop dan aplikasi mobile, dan GitHub Enterprise, belum pernah lebih mudah bagi individu dan tim untuk menulis kode yang lebih baik, lebih cepat.
-
 
 [GOAL](http://i.goal.com/id-ID/)
 
@@ -44,8 +38,5 @@ Dengan fitur kolaboratif GitHub.com, desktop dan aplikasi mobile, dan GitHub Ent
  // http://cariprogram.blogspot.com
  // http://i.goal.com/id-ID/
  
- //$feed = file_get_contents($_GET['url']);
- $xml = new SimpleXmlElement($feed, LIBXML_NOCDATA);
-
 ?>
 <?php
